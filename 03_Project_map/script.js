93fac445f581c79a5567d552a9d7a37c55bc1b8a
unlin <unlinun@hotmail.com>
2022-07-15 14:48:11 +0800

    finished load img and localStorage setting

--- 03_Project_map/script.js
+++ 03_Project_map/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const projectsWrapper = document.querySelector(".projects");
 const form = document.querySelector(".form");
 const input_type = document.querySelector(".input-type");
 const input_projectName = document.querySelector(".input-name");
@@ -10,7 +11,7 @@ const submit = document.querySelector(".add-btn");
 // 設定 APP class
 class Projects {
   date = new Date();
-  id = (Date.now() + " ").slice(-4);
+  id = (Date.now() + "").slice(-4);
   constructor(coords, type, projectName, projectImg, year) {
     this.coords = coords;
     this.type = type;
@@ -29,10 +30,30 @@ class App {
   //新增區域變量
   #map;
   #mapEvent;
+  #imgUrl;
   #projects = [];
   constructor() {
+    //先取得使用者位置
     this._getCurrentLocation();
+    //取得本地儲存資訊，並顯示在地圖上
+    this._getLocalStorage();
+    //當按下 esc 關閉 form
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "Escape") {
+        form.classList.add("hidden");
+        //clear value
+        input_projectImg.value =
+          input_projectName.value =
+          input_year.value =
+            "";
+      }
+    });
+    // form按下送出後，獲取作品資訊與呈現在地圖上
     form.addEventListener("submit", this._submitForm.bind(this));
+    //取得作品 url
+    input_projectImg.addEventListener("change", this._readURL.bind(this));
+    //點按作品時可以移動至該專案位置
+    projectsWrapper.addEventListener("click", this._moveMap.bind(this));
   }
 
   //獲取用戶當前位置
@@ -52,10 +73,9 @@ class App {
     //利用現在正確位置取得精度及緯度
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    const coords = [latitude, longitude];
-
+    const coord = [latitude, longitude];
     // 導入 leaflet API，並傳入經緯度
-    this.#map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coord, 13);
 
     L.tileLayer(
       "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
@@ -65,8 +85,11 @@ class App {
       }
     ).addTo(this.#map);
 
+    //當地圖載入完成後，載入marker
+    this.#projects.forEach((p) => this._renderMarker(p));
     //當點擊地圖時，加入 form 事件
     this.#map.on("click", this._showForm.bind(this));
+    this.#map.setView(coord, 5);
   }
 
   //show form
@@ -89,7 +112,7 @@ class App {
     //按下 + 按鈕，或是按下 enter 都可以
     const type = input_type.value;
     const projectName = input_projectName.value;
-    const projectImg = input_projectImg.value;
+    const projectImg = this.#imgUrl;
     const year = input_year.value;
     const { lat, lng } = this.#mapEvent.latlng;
     let project;
@@ -100,7 +123,6 @@ class App {
     } else {
       project = new Projects([lat, lng], type, projectName, projectImg, year);
     }
-
     // 新增一個新的 project 至 Projects
     this.#projects.push(project);
     // 將 marker 新增至地圖
@@ -109,31 +131,30 @@ class App {
     this._hideForm();
     // 將 project 呈現
     this._renderProject(project);
-    //將img 呈現
-    // this._renderImg(e);
+    console.log(this.#projects);
+    this._localStorage();
   }
 
   //在地圖上呈現 Marker
   _renderMarker(project) {
-    L.marker(project.coords)
+    project.marker = L.marker(project.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
           minWidth: 50,
-          maxWidth: 100,
+          maxWidth: 130,
           className: "stylePopup",
         })
       )
-      .setPopupContent(`${project.description}`)
-      .openPopup();
+      .setPopupContent(`${project.description}`);
+    //   .openPopup();
   }
 
   // 旁邊列出表格視窗
   _renderProject(project) {
     let html = `
-    <li class="project" data-id="223445">
+    <li class="project" data-id="${project.id}">
                         <div class="project-detial project-img">
-                            <img class="img" src="img.jpg" alt="">
                         </div>
                         <div class="project-detial">
                             <span class="project-title">Project name :</span>
@@ -149,13 +170,64 @@ class App {
                         </div>
                     </li>`;
     form.insertAdjacentHTML("afterend", html);
+    //將img 呈現
+    this._renderImg(project);
   }
-  _renderImg(e) {
+  //讀取圖片 URL
+  _readURL(event) {
     const reader = new FileReader();
-    reader.onload = function () {
-      const img = document.querySelector(".img");
-      img.src = URL.createObjectURL(e.target.files[0]);
+    reader.addEventListener("load", () => {
+      this.#imgUrl = reader.result;
+    });
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  //將圖片呈現於網頁
+  _renderImg(project) {
+    const image = document.querySelector(".project-img");
+    image.style.backgroundImage = `url(${project.projectImg})`;
+  }
+
+  //移動地圖
+  _moveMap(e) {
+    const mapMove = e.target.closest(".project");
+    if (!mapMove) return;
+    const project = this.#projects.find((p) => p.id === mapMove.dataset.id);
+    project.marker.openPopup(project.coords);
+    this.#map.setView(project.coords, 13);
+  }
+
+  //儲存在locaol storage
+  _localStorage() {
+    const getCircularReplacer = () => {
+      const seen = new WeakSet();
+      return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+          if (seen.has(value)) {
+            return;
+          }
+          seen.add(value);
+        }
+        return value;
+      };
     };
+    localStorage.setItem(
+      "projects",
+      JSON.stringify(this.#projects, getCircularReplacer())
+    );
+  }
+  //取得本地儲存資料
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("projects"));
+    if (!data) return;
+    this.#projects = data;
+    this.#projects.forEach((p) => this._renderProject(p));
+  }
+
+  //重設本地儲存資料
+  reset() {
+    localStorage.removeItem("projects");
+    location.reload();
   }
 }
 
