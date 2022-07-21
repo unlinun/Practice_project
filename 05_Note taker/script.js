2013f16280d9d8666deac47e58ec90c33ec43bd2
unlin <unlinun@hotmail.com>
2022-07-21 10:49:46 +0800

    finished popup window

--- 05_Note taker/script.js
+++ 05_Note taker/script.js
@@ -3,8 +3,14 @@
 const text = document.querySelector(".text");
 const item = document.querySelector(".note-item");
 const submitBtn = document.querySelector(".submit");
+const closeBtn = document.querySelector(".close");
+const popupBox = document.querySelector(".pop-up-box");
+const popupText = document.querySelector(".popup-text");
+const popupTitle = document.querySelector(".popup-title");
 
 class Note {
+  now = new Date();
+  id = (Date.now() + "").slice(-5);
   constructor(text) {
     this.text = text;
     this.title = text.slice(0, 5);
@@ -29,10 +35,19 @@ class App {
     ////renderNote
     this._renderNotes(note);
     this._clearValue();
+
+    ////////popup
+    const load = document.querySelectorAll(".load");
+    load.forEach((btn) =>
+      btn.addEventListener("click", this._popup.bind(this))
+    );
+
+    ////close popup
+    closeBtn.addEventListener("click", this._closePopup.bind(this));
   }
   ///// renderNote
   _renderNotes(note) {
-    const html = `<div class="item">
+    const html = `<div class="item" data-id="${note.id}">
     <h3 class="title">${note.title}</h3>
       <span class="note-text"
         >${note.text}</span
@@ -41,10 +56,6 @@ class App {
   </div>
     `;
     item.insertAdjacentHTML("beforeend", html);
-
-    //////load btn
-    // const load = document.querySelector(".load");
-    // load.addEventListener("click", this._popup.bind(this, note));
   }
   //////clear value
   _clearValue() {
@@ -52,26 +63,18 @@ class App {
   }
 
   /////// window pop-up
-  //   _popup(note) {
-  //     const html = `<section class="pop-up-box">
-  //     <div class="item pop-up-item">
-  //       <h3 class="title">${note.title}</h3>
-  //         <span class="note-text"
-  //           >${note.text}</span
-  //         >
-  //       <button class="close">x</button>
-  //     </div>
-  //   </section>
-  //     `;
-  //     item.insertAdjacentHTML("afterend", html);
-  //     //////close
-  //     const close = document.querySelector(".close");
-  //     close.addEventListener("click", function (e) {
-  //       const item = e.target;
-  //       const itemBox = item.parentElement.parentElement;
-  //       itemBox.remove();
-  //     });
-  //   }
+  _popup(e) {
+    e.preventDefault();
+    const item = e.target.parentElement;
+    const popupItem = this.#notes.find((n) => n.id === item.dataset.id);
+    popupBox.classList.remove("hidden");
+    popupTitle.textContent = `${popupItem.title}`;
+    popupText.textContent = `${popupItem.text}`;
+  }
+
+  _closePopup() {
+    popupBox.classList.add("hidden");
+  }
 }
 
 const app = new App();
