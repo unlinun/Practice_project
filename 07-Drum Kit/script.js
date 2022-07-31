e61f4358c7ea529708e6a17a0c6154b3feada475
unlinun <100119316+unlinun@users.noreply.github.com>
2022-07-31 15:30:34 +0800

    Update script.js
    add click event and keydown event

--- 07-Drum Kit/script.js
+++ 07-Drum Kit/script.js
@@ -1,6 +1,28 @@
 "use strict";
 const key = document.querySelectorAll(".key");
 
-key.forEach((k) => k.addEventListener("click", activePlaying));
 
-const activePlaying = function (e) {};
+const activePlaying = function (e) {
+    e.stopPropagation();
+    // console.log(e.target)
+    let keycode = e.keyCode || this.getAttribute('data-key')
+    // console.log(keycode)
+    const audio = document.querySelector(`audio[data-key="${keycode}"]`)
+    if(!audio) return; //stop the event
+    audio.currentTime =0;
+    audio.play();
+    const audioBox = document.querySelector(`.key[data-key="${keycode}"]`)
+    audioBox.classList.add('playing')
+    // console.log(audioBox)
+
+};
+
+const reomveTransition = function(e){
+    // console.log(e)
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+window.addEventListener('keydown',activePlaying)
+key.forEach(k=> k.addEventListener('click',activePlaying))
+key.forEach((k) => k.addEventListener("transitionend", reomveTransition));
