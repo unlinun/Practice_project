5bc573aa8b4cc48b917d95cdc124408999393db6
unlin <unlinun@hotmail.com>
2022-09-01 22:24:30 +0800

    Add grid system and pefer-color-scheme

--- 12_DarkLight_theme/src/js/script.js
+++ 12_DarkLight_theme/src/js/script.js
@@ -1,4 +1,35 @@
 "use strict";
 
-console.log("hello world");
-console.log("hello");
+// DOM select
+const html = document.querySelector("html");
+const input = document.querySelectorAll(`input[type="radio"]`);
+
+// btn
+const switcher = document.querySelector(".toggle__switcher");
+console.log(window.matchMedia(`(prefers-color-scheme: light)`));
+// load page set default theme
+
+// default theme ,  !!! USE matches !!!
+let defaultTheme = window.matchMedia(`(prefers-color-scheme: light)`).matches
+  ? "light"
+  : "dark";
+console.log(defaultTheme);
+// load event
+window.addEventListener("load", getTheme);
+function getTheme(e) {
+  html.setAttribute("data-theme", defaultTheme);
+  console.log(html);
+}
+
+let newTheme;
+input.forEach((i) => i.addEventListener("click", changeTheme));
+function changeTheme(e) {
+  newTheme = e.target.id;
+  console.log(newTheme);
+  if (newTheme == "default") {
+    html.setAttribute("data-theme", defaultTheme);
+  } else {
+    html.removeAttribute("data-theme");
+    html.setAttribute("data-theme", newTheme);
+  }
+}
