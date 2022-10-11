bf5089e9f517f8bdb36f3b80082c278bd21a8946
unlin <unlinun@hotmail.com>
2022-10-11 23:08:44 +0800

    update

---
+++ 21_ToDolist/README.md
@@ -0,0 +1,9 @@
+# Project Overview
+
+這是一個 to do list 的專案，使用者必須可以:
+
+1. 進行黑白主題的變換 (white & dark mode)
+2. 可以增加、編輯、刪除
+3. 畫面上會呈現任務的優先順序
+4. 可以用專案來進行分類
+5. 使用 localStorage
--- 21_ToDolist/src/index.js
+++ 21_ToDolist/src/index.js
@@ -1,5 +1,17 @@
 import "./scss/style.scss";
 import logo from "./assets/logo.svg";
 import menu from "./assets/align-justify.svg";
-import x from "./assets/x.svg";
 import user from "./assets/user.svg";
+import * as model from "./js/model";
+import addNewTaskView from "./js/View/addTaskView";
+
+// Add new task and pass data to model.js
+const controlAddNewTaskData = function (data) {
+  const newTasks = model.updateTaskState(data);
+  console.log(newTasks);
+};
+
+const init = function () {
+  addNewTaskView.addHandlerAddTaskData(controlAddNewTaskData);
+};
+init();
